e4858c7db00014567da9f5dc3bcec5f598c0baec
yunus ozen <yunus@yunus.gen.tr>
2023-02-19 18:41:21 +0300

    sleep stage R added. Polished.

--- read_edf.py
+++ read_edf.py
@@ -9,7 +9,6 @@ print(data.info.ch_names)
 
 annots = mne.read_annotations(file, uint16_codec = "utf8").to_data_frame()
 
-options=['Sleep stage W','Sleep stage N1','Sleep stage N2','Sleep stage N3']
-df2=annots[annots['description'].isin(options)]
-print(df2)
-print("--")
+options=['Sleep stage W','Sleep stage N1','Sleep stage N2','Sleep stage N3', 'Sleep stage R']
+df=annots[annots['description'].isin(options)]
+print(df)
