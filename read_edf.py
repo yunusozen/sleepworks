f6cfd3c0ba2b7fd8b5b1d4ea4325ec405e426e35
goksuozen43 <goksu2013.12@gmail.com>
2023-02-19 21:04:13 +0300

    read_edf.py file updated

--- read_edf.py
+++ read_edf.py
@@ -9,4 +9,6 @@ print(data.info.ch_names)
 
 annots = mne.read_annotations(file, uint16_codec = "utf8").to_data_frame()
 
-print(annots.head())
+options=['Sleep stage W','Sleep stage N1','Sleep stage N2','Sleep stage N3']
+df2=annots[annots['description'].isin(options)]
+print(df2)
