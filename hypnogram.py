629a535f0bbcdaf273e665d23ddc945be151ebcf
yunus ozen <yunus@yunus.gen.tr>
2023-02-25 19:55:34 +0300

    read_egg_from_edf and resample_eeg functions created

--- hypnogram.py
+++ hypnogram.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt, pandas as pd, numpy as np
 import random
 import read_edf
 
-lst=read_edf.read_edf()
+lst=read_edf.read_sleep_stages("data.edf")
 
 mapping = {'Sleep stage N3': 0, 'Sleep stage N2': 1, 'Sleep stage N1': 2, 'Sleep stage R': 3, 'Sleep stage W': 4}
 
--- read_edf.py
+++ read_edf.py
@@ -1,18 +1,51 @@
 
 import mne
 import pandas
+import numpy as np
+from scipy import signal
 
-def read_edf():
+def print_edf_info(edf_file_path='data.edf'):
 
-    file = "data.edf"
-    data = mne.io.read_raw_edf(file)
+    data = mne.io.read_raw_edf(edf_file_path)
 
     print(data.info)
     print(data.info.ch_names)
 
-    annots = mne.read_annotations(file, uint16_codec = "utf8").to_data_frame()
+
+def read_sleep_stages(edf_file_path='data.edf'):
+
+    annots = mne.read_annotations(edf_file_path, uint16_codec = "utf8").to_data_frame()
 
     options=['Sleep stage W','Sleep stage N1','Sleep stage N2','Sleep stage N3', 'Sleep stage R']
+
     df=annots[annots['description'].isin(options)]
+
     df=df['description'].tolist()
+
     return df
+
+
+def read_egg_from_edf(edf_file_path='data.edf', ch = 'Fpz'):
+
+    raw = mne.io.read_raw_edf(edf_file_path, preload=True)
+    
+    fpz_data, times = raw.get_data(picks=ch, return_times=True)
+
+    return fpz_data[0].tolist()
+
+
+
+
+def resample_eeg(eeg_data, old_fs=512, new_fs=100):
+
+    resampling_factor = new_fs / old_fs
+    
+    new_length = int(len(eeg_data) * resampling_factor)
+
+    # new_times = np.arange(new_length) / new_fs
+    
+    resampled_eeg_data = signal.resample(eeg_data, new_length)
+    
+    return resampled_eeg_data.tolist()
+
+
